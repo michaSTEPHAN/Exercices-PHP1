c99db1052b1c1cb751bfbba535e3501cafbb8219
michaSTEPHAN <167064870+michaSTEPHAN@users.noreply.github.com>
2024-04-15 16:43:03 +0200

    Update exo10.php

--- exo10.php
+++ exo10.php
@@ -13,30 +13,59 @@ echo "Montant versé : $MontantVersé €<br>";
 echo "A rendre : $MontantaRendre €<br>";
 echo "********************************************<br>";
 
-if ($MontantaRendre <= 0) {
-    echo "Il n'y a pas de monnaie à rendre <br>";
-} else {
-    $NbBillet10 = 0;
-    $NbBillet5  = 0;
-    $NbPiece2   = 0;
-    $NbPiece1   = 0;
+//--------------------------------------------------------------------------
+// VERSION AVEC INTDIV
+//--------------------------------------------------------------------------
+$NbBillet10 = 0;
+$NbBillet5  = 0;
+$NbPiece2   = 0;
+$NbPiece1   = 0;
 
-    while ($MontantaRendre > 0) {
-        if ($MontantaRendre > 10) {
-            $NbBillet10++;
-            $MontantaRendre-=10;
-        } elseif ($MontantaRendre > 5) {
-            $NbBillet5++;  
-            $MontantaRendre-=5; 
-        } elseif ($MontantaRendre > 2) {
-            $NbPiece2++;
-            $MontantaRendre-=2; 
-        } else {
-            $NbPiece1++; 
-            $MontantaRendre-=1; 
-        }    
-    }
-        echo "Rendu de monnaie :<br>";
-        echo "$NbBillet10 billets de 10 € - $NbBillet5 billets de 5 € - $NbPiece2 pièces de 2 € - $NbPiece1 pièces de 1 €<br>";    
-
+if (intdiv($MontantaRendre,10) > 0) {
+    $NbBillet10 = intdiv($MontantaRendre,10);
+    $MontantaRendre -= ($NbBillet10*10);
 }
+if (intdiv($MontantaRendre,5) > 0) {
+    $NbBillet5 = intdiv($MontantaRendre,5);
+    $MontantaRendre -= ($NbBillet5*5);
+}
+if (intdiv($MontantaRendre,2) > 0) {
+    $NbPiece2 = intdiv($MontantaRendre,2);
+    $MontantaRendre -= ($NbPiece2*2);
+}
+elseif (intdiv($MontantaRendre,1) > 0) {
+    $NbPiece1 = intdiv($MontantaRendre,1);
+    $MontantaRendre -= ($NbPiece1*1);
+}
+echo "Rendu de monnaie :<br>";
+echo "$NbBillet10 billets de 10 € - $NbBillet5 billets de 5 € - $NbPiece2 pièces de 2 € - $NbPiece1 pièces de 1 €<br>";    
+
+//--------------------------------------------------------------------------
+// "MA" VERSION AVEC WHILE
+//--------------------------------------------------------------------------
+// if ($MontantaRendre <= 0) {
+//     echo "Il n'y a pas de monnaie à rendre <br>";
+// } else {
+//     $NbBillet10 = 0;
+//     $NbBillet5  = 0;
+//     $NbPiece2   = 0;
+//     $NbPiece1   = 0;
+
+//     while ($MontantaRendre > 0) {
+//         if ($MontantaRendre > 10) {
+//             $NbBillet10++;
+//             $MontantaRendre-=10;
+//         } elseif ($MontantaRendre > 5) {
+//             $NbBillet5++;  
+//             $MontantaRendre-=5; 
+//         } elseif ($MontantaRendre > 2) {
+//             $NbPiece2++;
+//             $MontantaRendre-=2; 
+//         } else {
+//             $NbPiece1++; 
+//             $MontantaRendre-=1; 
+//         }    
+//     }
+//     echo "Rendu de monnaie :<br>";
+//     echo "$NbBillet10 billets de 10 € - $NbBillet5 billets de 5 € - $NbPiece2 pièces de 2 € - $NbPiece1 pièces de 1 €<br>";    
+// }
